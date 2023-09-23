a34e0a87f4c2f0a2198f6a3b6c6e312e333b2398
adypr <workady2015@gmail.com>
2023-09-23 22:29:53 +0300

    fix: edit path for dist folder

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default {
         ViteEjsPlugin(), 
     ],
     build: {
+        outDir: '../dist',
         rollupOptions: {
             input: {
                main: resolve(__dirname, 'src/index.html'),
